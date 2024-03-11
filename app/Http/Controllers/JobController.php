6f5bb2d6802e26c2b27592e4869336943836eb85
wstudionatali <wstudionatali@gmail.com>
2024-03-11 20:39:06 +0100

    Filtering Jobs: Min & Max Salary, experiens

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -15,8 +15,16 @@ class JobController extends Controller
         $jobs = Job::query();
 
         $jobs->when(request('search'), function ($query) {
-            $query->where('title', 'like', '%' . request('search') . '%')
+            $query->where( function ($query) {
+                $query->where('title', 'like', '%' . request('search') . '%')
                 ->orWhere('description', 'like', '%' . request('search') . '%');
+            });            
+        })->when(request('min_salary'), function ($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function ($query) {
+            $query->where('salary', '<=', request('max_salary'));
+        })->when(request('experience'), function ($query) {
+            $query->where('experience', request('experience'));
         });
 
         return view('job.index', ['jobs' => $jobs->get()]);
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -6,16 +6,44 @@
       <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
           <div class="mb-1 font-semibold">Search</div>
-          <x-text-input name="search" value="" placeholder="Search for any text" />
+          <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
         </div>
         <div>
           <div class="mb-1 font-semibold">Salary</div>
 
           <div class="flex space-x-2">
-            <x-text-input name="min_salary" value="" placeholder="From" />
-            <x-text-input name="max_salary" value="" placeholder="To" />
+            <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
+            <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
           </div>
         </div>
+        <div>
+          <div class="mb-1 font-semibold">Experience</div>
+
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value=""
+              @checked(!request('experience')) />
+            <span class="ml-2">All</span>
+          </label>
+
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value="entry"
+              @checked('entry' === request('experience')) />
+            <span class="ml-2">Entry</span>
+          </label>
+
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value="intermediate"
+              @checked('intermediate' === request('experience')) />
+            <span class="ml-2">Intermediate</span>
+          </label>
+
+          <label for="experience" class="mb-1 flex items-center">
+            <input type="radio" name="experience" value="senior"
+              @checked('senior' === request('experience')) />
+            <span class="ml-2">Senior</span>
+          </label>
+        </div>
+        <div>4</div>
       </div>
       <button class="w-full">Filter</button>
     </form>  
