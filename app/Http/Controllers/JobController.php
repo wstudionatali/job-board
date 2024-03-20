b67c80950e1574e35441675239c6cf74afbca56c
wstudionatali <wstudionatali@gmail.com>
2024-03-20 18:16:56 +0100

    searching by employer name, other employer jobs

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -43,7 +43,9 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        return view('job.show', compact('job'));
+        return view('job.show', 
+        ['job' => $job->load('employer.jobs')]
+        );
     }
 
     /**
