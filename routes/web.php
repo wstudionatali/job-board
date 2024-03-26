6ffc6bd011119d3e5a0956862a7a09ef6876efe9
wstudionatali <wstudionatali@gmail.com>
2024-03-26 21:41:27 +0100

    Start for creation authentication

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,5 +14,12 @@ use App\Http\Controllers\JobController;
 |
 */
 
+Route::get('', fn() => to_route('jobs.index'));
+
 Route::resource('jobs', JobController::class)
-    ->only(['index', 'show']);
+       ->only(['index', 'show']);
+
+Route::get('login', fn() => to_route('auth.create'))->name('login');
+Route::resource('auth', AuthController::class)
+       ->only(['create', 'store']);
+
